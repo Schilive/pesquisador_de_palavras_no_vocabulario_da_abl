ee3e2cc824c46b398683d6b10eeaee09477eebb3
Gabriel Silva Schilive <39244102+Schilive@users.noreply.github.com>
2020-05-02 19:58:24 -0300

    Projetado e com todos os arquivos necessários
    Os 2 arquivos de código (mais informação https://trello.com/b/NftPCyZq/pesquisador-no-dicion%C3%A1rio-da-abl) e o driver do Google Chrome que a biblioteca Selenium necessita foram adicionados.

---
+++ pesquisador.py
@@ -0,0 +1,10 @@
+def iniciar():
+    "Esta função abre o novegador com as necessárias configurações, acessa à página da ABL quista"
+
+
+def pesquisar(x: str) -> bool:
+    "É checado se a palavra contida em 'X' consta no vocabulário da ABL"
+
+
+def sair():
+    "O navegador e o programa são fechados"
---
+++ terminal.py
@@ -0,0 +1,9 @@
+"""
+Comandos do Terminal:
+
+iniciar (referente ao arquivo "pesquisador.py");
+pesquisar <x> (referente ao arquivo "pesquisador.py"), podendo <x> conter espaços;
+sair (referente ao arquivo "pesquisador.py");
+/? OU ? OU -? invocam o comando de ajuda.
+
+"""
