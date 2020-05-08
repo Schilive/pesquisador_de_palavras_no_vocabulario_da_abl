fa96ab38be1313e7469e85068e278a5e952750ab
GSS <schilive100dev@gmail.com>
2020-05-08 00:55:16 -0300

    Pesquisar() de Pesquisador.py foi totalmente remodelado, ao invés de "find", "wait" é usado, pela maior facilidade e um número massivo menor de erros, o que torna o programa mais estável e aparentemente mais rápido.
    Ò Terminal.py foi adicionado:
    	Pesquisa múltipla;
    	Barra de progresso à pesquisa múltipla;
    	Novo nome à pesquisa: "p";
    	Se 'CMD' for apenas espaços ou não contiver nada, é o mesmo de digitar um comando não reconhecido;
    	Se um comando não reconhecido for digitado, aparece uma mensagem no terminal.

--- Pesquisador.py
+++ Pesquisador.py
@@ -1,58 +1,66 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as ec
 
 
 def iniciar():
-    """Esta função abre o novegador com as necessárias configurações, acessa à página da ABL quista"""
+    """Abre o novegador com as necessárias configurações e acessa à página da ABL quista"""
 
     global browser
 
     options = Options()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    options.add_experimental_option("prefs", prefs)
     options.add_argument("--headless")  # Torna o navegador invisível
-    chrome_driver = "chromedriver_win32/chromedriver.exe"
+    chrome_driver = "D:/programatio/PycharmProjects/pesquisador_de_palavras_no_vocabulario_da_abl" \
+                    "/chromedriver_win32/chromedriver.exe"
     browser = webdriver.Chrome(executable_path=chrome_driver, chrome_options=options)
-    browser.implicitly_wait(10)
     browser.get("http://www.academia.org.br/print/nossa-lingua/busca-no-vocabulario")
 
 
-def pesquisar(x: str) -> bool:
-    """É checado se a palavra contida em 'X' consta no vocabulário da ABL"""
+def pesquisar(plvr: str, maxtempo: int = 5) -> bool:
+    """
+    É checado se a palavra contida em 'X' consta no vocabulário da ABL
 
-    # Procurar input para nele escrever a variável 'x'
-    findinput = browser.find_element_by_tag_name("input")  # Procura o input
-    findinput.send_keys(Keys.CONTROL, "a", Keys.DELETE)  # Deletar o texto escrito no input.
-    findinput.send_keys(x)  # Escreve a variável 'x'
+    PARÂMETROS:
+        plvr        - Requerida : palavra para ser pesquisada (str)
+        maxtempo    - Opcional  : tempo para o pesquisador perceber que a variável não consta (int)
+    """
 
-    # Procurar botão para apertá-lo
+    # digita e espera até a palavra "alhures" ser encontrada
+
+    # Procura input para escrever-lhe a variável 'x'
+    findinput = browser.find_element_by_tag_name("input")  # Procura o INPUT
+    findinput.clear()  # Apaga todos os textos contidos em INPUT
+    findinput.send_keys("alhures")  # Escreve "alhures"
+
+    # Procura botão para apertá-lo
     findbtn = browser.find_element_by_css_selector("button.btn.btn-primary")  # Procura o botão
     findbtn.click()  # Clica-lhe
 
-    # Procura todas as respostas dadas pelo site
-    browser.implicitly_wait(10)
-    for c in range(0, 5):
-        try:
-            findpalavra = browser.find_elements_by_class_name("item-palavra")
-        except NoSuchElementException:
-            return False
-        except StaleElementReferenceException:
-            print("BATATUS")
-            return False
+    # Espera até "alhures" ser encontrado
+    WebDriverWait(browser, 50).until(ec.presence_of_element_located((By.XPATH, "//strong[contains(.,'alhures')]")))
 
-        # Checa se uma das respostas é igual à variável 'x' e resolve o erro de "StaleElementReferenceException"
-        try:
-            for d in range(0, len(findpalavra)):
-                if findpalavra[c].text == x:
-                    return True
-                elif c == len(findpalavra) - 1:
-                    return False
-        except StaleElementReferenceException:
-            continue
+    # Digita e procura à palavra contida na varável 'x'
+
+    # Escreve a variável 'x' e clica no botão "pesquisar"
+    findinput.clear()
+    findinput.send_keys(plvr)
+
+    findbtn.click()
+
+    # Procura a palavra contida em 'x'
+    try:
+        WebDriverWait(browser, maxtempo).until(ec.presence_of_element_located((By.XPATH, f"//strong[contains(.,'{plvr}')]")))
+    except TimeoutException:
+        return False
+    return True
 
 
 def sair():
-    """O navegador e o programa são fechados"""
+    """O Pesquisador é fechado"""
 
     browser.quit()
--- terminal.py
+++ terminal.py
@@ -1,4 +1,22 @@
 import Pesquisador
+import time
+
+
+def print_barra_de_progresso(iteration, total):
+    """
+    Call in a loop to create terminal progress bar
+    @:param:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+    """
+    percent = ("{0:." + str(2) + "f}").format(100 * (iteration / float(total)))
+    filledlength = int(50 * iteration // total)
+    bar = '█' * filledlength + '-' * (50 - filledlength)
+    print(f'\rProgresso: |{bar}| {percent}% Completo', end="")
+    # Print New Line on Complete
+    if iteration == total:
+        print("\n")
+
 
 """
 Comandos do Terminal:
@@ -6,28 +24,35 @@ Comandos do Terminal:
 iniciar (referente ao arquivo "Pesquisador.py");
 pesquisar <palavra> (referente ao arquivo "Pesquisador.py"), podendo <palavra> conter espaços;
 sair (referente ao arquivo "Pesquisador.py");
-/? OU ? OU -? invocam o comando de ajuda.
+pesquisa múltipla | pesquisa mais duma palavra;
+/? OU ? OU -? OU ajuda OU help | invocam o comando de ajuda.
 
 """
 
 iniciado: bool = False  # Se o Pesquisador está iniciado
+maxtempo: int = 5  # Tempo máximo para o pesquisador perceber que a palavra não consta
 
 while True:
     cmd: str = input(">: ")  # Para o usuário digitar um comando
-
+    print(cmd)
     # Comandos
-    if cmd == "iniciar":
+    if cmd.isspace() or cmd == "":
+        print("Comando não identificado. Para ver os comandos disponíveis, digitar \"ajuda\".")
+    elif cmd == "iniciar":
         if not iniciado:
             Pesquisador.iniciar()
             iniciado = True
         else:
             print("O Pesquisador já está iniciado.")
-    elif cmd.split()[0] == "pesquisar":
+    elif cmd.split()[0] == "pesquisar" or cmd.split()[0] == "p":
         if iniciado and len(cmd.split()) > 1:
-            palavra: str = ""  # Variável da palavra a ser pesquisada
+            palavra: str = ""  # Variável que contem a palavra a ser pesquisada
 
-            for c in range(1, len(cmd.split())):
-                palavra = palavra + (cmd.split())[c]
+            for c in range(1, len(cmd.split())):  # Faz "p joão da cruz" e "p joão" em "joão da cruz" e "joão"
+                if c != 1:
+                    palavra = palavra + " " + (cmd.split())[c]
+                else:
+                    palavra = palavra + (cmd.split())[c]
 
             resultado = Pesquisador.pesquisar(palavra)
 
@@ -37,15 +62,59 @@ while True:
                 print(f"\"{palavra}\" não consta no vocabulário da ABL.")
 
         elif not iniciado:
-            print("O Pesquisador precisa ser iniciado. Para iniciá-lo, digite \"iniciar\".")
+            print("O Pesquisador precisa ser iniciado. Para iniciá-lo, digitar \"iniciar\".")
         else:
             print("O comando \"pesquisar <palavra>\" precisa de pelo menos uma palavra.")
+    elif cmd.split()[0] == "pesquisa múltipla" or cmd.split()[0] == "pesquisa multipla" or cmd.split()[0] == "pm":
+        if iniciado and len(cmd.split()) > 1:
+            palavras: list = [""]
+
+            e: int = 0
+            for f in range(1, len(cmd.split())):
+                if cmd.split()[f][-1] == ",":
+                    palavras[e] = palavras[e] + cmd.split()[f][:-1]
+                    e = e + 1
+                    palavras.append("")
+                else:
+                    if f == len(cmd.split()) - 1:
+                        palavras[e] = palavras[e] + cmd.split()[f]
+                    else:
+                        palavras[e] = palavras[e] + cmd.split()[f] + " "
+
+            resultados = []
+
+            print()
+            for f in range(0, len(palavras)):
+                palavralocal = palavras[f]
+                resultados.append(Pesquisador.pesquisar(palavralocal))
+                time.sleep(0.1)
+                print_barra_de_progresso(f + 1, len(palavras))
+            for g in range(0, len(palavras)):  # dá "print" de "<palavra> <consta/não está contido> no vocabulário
+                # da ABL"
+                if g != len(palavras) - 1:  # não for o último
+                    if resultados[g]:
+                        print(f"\"{palavras[g]}\" consta no vocabulário da ABL;")
+                    else:
+                        print(f"\"{palavras[g]}\" não está contido no vocabulário da ABL;")
+                else:
+                    if resultados[g]:
+                        print(f"\"{palavras[g]}\" consta no vocabulário da ABL.\n")
+                    else:
+                        print(f"\"{palavras[g]}\" não está contido no vocabulário da ABL.\n")
+
+        elif not iniciado:
+            print("O Pesquisador precisa ser iniciado. Para iniciá-lo, digitar \"iniciar\".")
+        else:
+            print("O comando \"pm <palavra1>; <palavra2>\" precisa de pelo menos uma palavra.")
     elif cmd == "sair":
         Pesquisador.sair()
         break
-    elif cmd == "/?" or cmd == "/?" or cmd == "-?":
+    elif cmd == "/?" or cmd == "/?" or cmd == "-?" or cmd == "ajuda" or cmd == "help":
         print("""Comandos do Terminal:
         iniciar | Inicia o 'Pesquisador';
-        pesquisar <palavra> | Pesquisa a <palavra> para dizer se conta ou não no vocabulário da ABL;
+        pesquisar OU p <palavra> | Pesquisa a <palavra> para dizer se conta ou não no vocabulário da ABL;
+        pesquisa múltipla OU pesquisa multipla OU pm <palavra1>, <palavra2> | Pesquisa 1 ou mais palavras;
         sair | Fecha o 'Pesquisador' e o Terminal;
-        /? OU ? OU -? | Abrem a ajuda do Terminal.""")
+        /? OU ? OU -? OU ajuda OU help | Abrem a ajuda do Terminal.""")
+    else:
+        print("Comando não identificado. Para ver os comandos disponíveis, digitar \"ajuda\"")
